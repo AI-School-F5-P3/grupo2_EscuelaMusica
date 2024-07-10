b1f56f9853774fe9dae0dcdfecc2328c5a4ef143
Jaanh YB <jyajuber@gmail.com>
2024-07-10 15:52:02 +0200

    Delete app directory

--- app/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://user:password@localhost/grupo2_EscuelaMusica')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
