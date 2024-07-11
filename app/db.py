bd5fb80b02fc47f28d659dc180e18bb965006d42
alepinb <alepinangob@gmail.com>
2024-07-11 15:56:06 +0200

    actulizar nombre de base de datos

--- app/db.py
+++ app/db.py
@@ -1,12 +1,12 @@
 # app/db.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.models import Base
+from models import db
 
 # Configura la conexión a la base de datos
-engine = create_engine('mysql://username:password@localhost/ArmaniaUtopia')
+engine = create_engine('mysql://username:password@localhost/ArmoniaUtopia')
 Session = sessionmaker(bind=engine)
 
 # Crea las tablas en la base de datos si no existen
-Base.metadata.create_all(engine)
+db.metadata.create_all(engine)
 
