81135ae7e82654ec4494a952cde5fede31e53cb9
Jaanh YB <jyajuber@gmail.com>
2024-07-12 11:45:49 +0200

    Update db.py

--- app/db.py
+++ app/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from models import db
 
 # Configura la conexión a la base de datos
-engine = create_engine('mysql://username:password@localhost/ArmoniaUtopia')
+engine = create_engine('mysql://username:password@localhost/armonia_utopia')
 Session = sessionmaker(bind=engine)
 
 # Crea las tablas en la base de datos si no existen
