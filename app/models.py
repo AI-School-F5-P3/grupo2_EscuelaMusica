ef9d4c355f61aba3f4380d5b37bb3ed69c252a6e
Jaanh YB <jyajuber@gmail.com>
2024-07-14 15:19:46 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,9 +4,10 @@ from sqlalchemy import Column, Integer, String, Boolean, Float, Enum, ForeignKey
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from faker import Faker #pip install faker
+from __init__ import db
 
-Base = declarative_base()
-class Student(Base):
+db.Model = declarative_base()
+class Student(db.Model):
     __tablename__ = 'students'
 
     id_student = Column(Integer, primary_key=True, autoincrement=True)
@@ -19,7 +20,7 @@ class Student(Base):
 
     enrollments = relationship('Enrollment', backref='student')
 
-class Teacher(Base):
+class Teacher(db.Model):
     __tablename__ = 'teachers'
 
     id_teacher = Column(Integer, primary_key=True, autoincrement=True)
@@ -30,7 +31,7 @@ class Teacher(Base):
     instruments = relationship('Instrument', secondary='teachers_instruments',backref='teacher')
 
     #instruments = relationship('TeacherInstrument', backref='teacher')
-class Level(Base):
+class Level(db.Model):
     __tablename__ = 'levels'
 
     id_level = Column(Integer, primary_key=True, autoincrement=True)
@@ -38,7 +39,7 @@ class Level(Base):
 
     instruments = relationship('InstrumentLevel', backref='level')
 
-class Instrument(Base):
+class Instrument(db.Model):
     __tablename__ = 'instruments'
 
     id_instrument = Column(Integer, primary_key=True, autoincrement=True)
@@ -50,14 +51,14 @@ class Instrument(Base):
     instrument_levels = relationship('Level',secondary="instruments_levels", backref='instruments')
     #enrollments = relationship('Enrollment', backref='instrument')
 
-class TeacherInstrument(Base):
+class TeacherInstrument(db.Model):
     __tablename__ = 'teachers_instruments'
     
     id_teacher = Column('id_teacher',Integer, ForeignKey('teachers.id_teacher'), primary_key=True)
     id_instrument = Column('id_instrument',Integer, ForeignKey('instruments.id_instrument'), primary_key=True)
     #id_level = Column(Integer, ForeignKey('levels.id_level'))
 
-class Enrollment(Base):
+class Enrollment(db.Model):
     __tablename__ = 'enrollments'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -69,7 +70,7 @@ class Enrollment(Base):
     final_price = Column(Float)
     family_discount = Column(Boolean)
 
-class PriceInstrument(Base):
+class PriceInstrument(db.Model):
     __tablename__ = 'price_instrument'
 
     id_price = Column(Integer, primary_key=True, autoincrement=True)
@@ -77,7 +78,7 @@ class PriceInstrument(Base):
     pack_price = Column(Float)
 
     #enrollments = relationship('Enrollment', secondary='price_instrument_enrollments', backref='price_instrument')
-class Discount(Base):
+class Discount(db.Model):
     __tablename__ = 'discount'
 
     id_discount = Column(Integer, primary_key=True, autoincrement=True)
@@ -87,21 +88,20 @@ class Discount(Base):
 
     enrollments = relationship('Enrollment', secondary='discount_enrollments', backref='discount')
 
-class InstrumentLevel(Base):
+class InstrumentLevel(db.Model):
     __tablename__ = 'instruments_levels'
 
     id_instrument = Column(Integer, ForeignKey('instruments.id_instrument'), primary_key=True)
     id_level = Column(Integer, ForeignKey('levels.id_level'), primary_key=True)
 
-class PriceInstrumentEnrollment(Base):
+class PriceInstrumentEnrollment(db.Model):
     __tablename__ = 'price_instrument_enrollments'
 
     price_instrument_pack = Column(Enum('pack1', 'pack2', 'pack3'), primary_key=True)
     enrollments_base_price = Column(Float, primary_key=True)
 
-class DiscountEnrollment(Base):
+class DiscountEnrollment(db.Model):
     __tablename__ = 'discount_enrollments'
 
     discount_discount_percentage = Column(Float, primary_key=True)
     enrollments_final_price = Column(Float, primary_key=True)
-
