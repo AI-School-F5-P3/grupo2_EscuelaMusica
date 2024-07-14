429ed23803634a3a577a70bc58db80782835765e
fjmc <jmc.freddy1@gmail.com>
2024-07-14 10:20:11 -0700

    actualizar

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\fjmc0\Desktop\escuela_musica\grupo2_EscuelaMusica\venv
+set VIRTUAL_ENV=C:\Users\Administrator\Desktop\school_music\grupo2_EscuelaMusica\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
