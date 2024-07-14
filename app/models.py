8bb1520eaa38dd24c9269b08e38d8cf07885bd9e
Jaanh YB <jyajuber@gmail.com>
2024-07-14 13:01:35 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,99 +1,107 @@
-from app import db
+from tabulate import tabulate
+from sqlalchemy import create_engine #Se usa para crear un motor de bbdd
 from sqlalchemy import Column, Integer, String, Boolean, Float, Enum, ForeignKey, create_engine
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from faker import Faker #pip install faker
 
-class Student(db.Model):
+Base = declarative_base()
+class Student(Base):
     __tablename__ = 'students'
 
-    id_student = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    first_name = db.Column(db.String(255))
-    last_name = db.Column(db.String(255))
-    age = db.Column(db.Integer)
-    phone = db.Column(db.String(255))
-    email = db.Column(db.String(255))
+    id_student = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(20))
+    last_name = Column(String(20))
+    age = Column(Integer)
+    phone = Column(String(20))
+    email = Column(String(20))
+    #enrollments = relationship('Enrollment', backref='student')
 
-    enrollments = db.relationship('Enrollment', backref='student')
+    enrollments = relationship('Enrollment', backref='student')
 
-class Teacher(db.Model):
+class Teacher(Base):
     __tablename__ = 'teachers'
 
-    id_teacher = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name_teacher = db.Column(db.String(255), nullable=False)
+    id_teacher = Column(Integer, primary_key=True, autoincrement=True)
+    name_teacher = Column(String(20))
+    last_name=Column(String(20))
+    telphone = Column(String(20))
+    email = Column(String(20))
+    instruments = relationship('Instrument', secondary='teachers_instruments',backref='teacher')
 
-    instruments = db.relationship('TeacherInstrument', backref='teacher')
-
-class Level(db.Model):
+    #instruments = relationship('TeacherInstrument', backref='teacher')
+class Level(Base):
     __tablename__ = 'levels'
 
-    id_level = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name_level = db.Column(db.String(255))
+    id_level = Column(Integer, primary_key=True, autoincrement=True)
+    name_level = Column(String(25))
 
-    instruments = db.relationship('InstrumentLevel', backref='level')
+    instruments = relationship('InstrumentLevel', backref='level')
 
-class Instrument(db.Model):
+class Instrument(Base):
     __tablename__ = 'instruments'
 
-    id_instrument = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    instrument = db.Column(db.String(255), nullable=False)
+    id_instrument = Column(Integer, primary_key=True, autoincrement=True)
+    instrument = Column(String(20), nullable=False)
 
-    teacher_instruments = db.relationship('TeacherInstrument', backref='instrument')
-    instrument_levels = db.relationship('InstrumentLevel', backref='instrument')
-    enrollments = db.relationship('Enrollment', backref='instrument')
+    teacher_instruments = relationship('TeacherInstrument', backref='instrument')
+    
+    
+    instrument_levels = relationship('Level',secondary="instruments_levels", backref='instruments')
+    #enrollments = relationship('Enrollment', backref='instrument')
 
-class TeacherInstrument(db.Model):
+class TeacherInstrument(Base):
     __tablename__ = 'teachers_instruments'
+    
+    id_teacher = Column('id_teacher',Integer, ForeignKey('teachers.id_teacher'), primary_key=True)
+    id_instrument = Column('id_instrument',Integer, ForeignKey('instruments.id_instrument'), primary_key=True)
+    #id_level = Column(Integer, ForeignKey('levels.id_level'))
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id_teacher = db.Column(db.Integer, db.ForeignKey('teachers.id_teacher'))
-    id_instrument = db.Column(db.Integer, db.ForeignKey('instruments.id_instrument'))
-    id_level = db.Column(db.Integer, db.ForeignKey('levels.id_level'))
-
-class Enrollment(db.Model):
+class Enrollment(Base):
     __tablename__ = 'enrollments'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id_student = db.Column(db.Integer, db.ForeignKey('students.id_student'))
-    id_level = db.Column(db.Integer, db.ForeignKey('levels.id_level'))
-    id_instrument = db.Column(db.Integer, db.ForeignKey('instruments.id_instrument'))
-    id_teacher = db.Column(db.Integer, db.ForeignKey('teachers.id_teacher'))
-    base_price = db.Column(db.Float)
-    final_price = db.Column(db.Float)
-    family_discount = db.Column(db.Boolean)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    id_student = Column(Integer, ForeignKey('students.id_student'))
+    id_level = Column(Integer, ForeignKey('levels.id_level'))
+    id_instrument = Column(Integer, ForeignKey('instruments.id_instrument'))
+    id_teacher = Column(Integer, ForeignKey('teachers.id_teacher'))
+    base_price = Column(Float)
+    final_price = Column(Float)
+    family_discount = Column(Boolean)
 
-class PriceInstrument(db.Model):
+class PriceInstrument(Base):
     __tablename__ = 'price_instrument'
 
-    id_price = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    pack = db.Column(Enum('pack1', 'pack2', 'pack3'))
-    pack_price = db.Column(db.Float)
+    id_price = Column(Integer, primary_key=True, autoincrement=True)
+    pack = Column(String(10))
+    pack_price = Column(Float)
 
-    enrollments = db.relationship('Enrollment', secondary='price_instrument_enrollments', backref='price_instrument')
-
-class Discount(db.Model):
+    #enrollments = relationship('Enrollment', secondary='price_instrument_enrollments', backref='price_instrument')
+class Discount(Base):
     __tablename__ = 'discount'
 
-    id_discount = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    group_discount = db.Column(Enum('pack1', 'pack2', 'pack3'))
-    count_instrument = db.Column(db.Integer)
-    discount_percentage = db.Column(db.Float)
+    id_discount = Column(Integer, primary_key=True, autoincrement=True)
+    group_discount = Column(Enum('pack1', 'pack2', 'pack3'))
+    count_instrument = Column(Integer)
+    discount_percentage = Column(Float)
 
-    enrollments = db.relationship('Enrollment', secondary='discount_enrollments', backref='discount')
+    enrollments = relationship('Enrollment', secondary='discount_enrollments', backref='discount')
 
-class InstrumentLevel(db.Model):
+class InstrumentLevel(Base):
     __tablename__ = 'instruments_levels'
 
-    instruments_id_instrument = db.Column(db.Integer, db.ForeignKey('instruments.id_instrument'), primary_key=True)
-    levels_id_level = db.Column(db.Integer, db.ForeignKey('levels.id_level'), primary_key=True)
+    id_instrument = Column(Integer, ForeignKey('instruments.id_instrument'), primary_key=True)
+    id_level = Column(Integer, ForeignKey('levels.id_level'), primary_key=True)
 
-class PriceInstrumentEnrollment(db.Model):
+class PriceInstrumentEnrollment(Base):
     __tablename__ = 'price_instrument_enrollments'
 
-    price_instrument_pack = db.Column(Enum('pack1', 'pack2', 'pack3'), primary_key=True)
-    enrollments_base_price = db.Column(db.Float, primary_key=True)
+    price_instrument_pack = Column(Enum('pack1', 'pack2', 'pack3'), primary_key=True)
+    enrollments_base_price = Column(Float, primary_key=True)
 
-class DiscountEnrollment(db.Model):
+class DiscountEnrollment(Base):
     __tablename__ = 'discount_enrollments'
 
-    discount_discount_percentage = db.Column(db.Float, primary_key=True)
-    enrollments_final_price = db.Column(db.Float, primary_key=True)
+    discount_discount_percentage = Column(Float, primary_key=True)
+    enrollments_final_price = Column(Float, primary_key=True)
+
