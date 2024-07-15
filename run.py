e40549f73fcc86b048dfcc0c856d58e9afd28d66
Jaanh <jyajuber@gmail.com>
2024-07-15 14:21:47 +0200

    update models

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
-from app import app, db
+from app import create_app, db
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     print("Base de datos inicializada correctamente.")
-    app.run(debug=True)
+    app.run()
