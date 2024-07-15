0e9c6394f149be41bcbf1db315983f7036ea601c
Jaanh <jyajuber@gmail.com>
2024-07-15 23:12:06 +0200

    update

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from app.models import db, init_db
 from app.frontend_routes import frontend_bp  # Importa el Blueprint
+import os
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:"Fausto-007"@localhost:3306/armonia_utopia'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI','mysql+pymysql://root:Fausto-007@localhost:3306/armonia_utopia')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.register_blueprint(frontend_bp)
     db.init_app(app)
