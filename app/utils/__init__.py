fc5cfc8d36990041f7177343ffe35c9dfb155cfa
Jaanh YB <jyajuber@gmail.com>
2024-07-16 13:41:13 +0200

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,4 @@
 from .csv_export import export_to_csv
 from .exceptions import ResourceNotFoundError
-from .logging import setup_logging
+from .app_logging import setup_logger, log_request, log_error, log_info, log_warning, log_debug
 from .security import generate_password_hash, check_password_hash
