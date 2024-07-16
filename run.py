8b66f2864f755947c79b6e9a9a5a640ec866cb35
alepinb <alepinangob@gmail.com>
2024-07-16 19:27:43 +0200

    actulizar rama

--- run.py
+++ run.py
@@ -1,22 +1,8 @@
 from flask import Flask
-from app.models import db, init_db
-from urllib.parse import quote_plus
+from app import create_app
 
-password = quote_plus("rocio99")
-from app.frontend_routes import frontend_bp  # Importa el Blueprint
 
-def create_app():
-    app = Flask(__name__)
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{password}@localhost:3306/armonia_utopia'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.register_blueprint(frontend_bp)
-    db.init_app(app)
-    
-    with app.app_context():
-        init_db(app)
-     
-    return app
+
 
 if __name__ == "__main__":
     app = create_app()
