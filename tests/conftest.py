2fd8b379ae27b16802462d41834f8302f8415ff7
Jaanh YB <jyajuber@gmail.com>
2024-07-16 13:20:31 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
 import pytest
 from app import create_app, db
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 @pytest.fixture(scope='module')
 def app():
     app = create_app('testing')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('TEST_DATABASE_URI', 'mysql+pymysql://test_user:test_password@localhost:3306/test_armonia_utopia')
+    
     with app.app_context():
         db.create_all()
         yield app
@@ -13,3 +19,11 @@ def app():
 @pytest.fixture(scope='module')
 def client(app):
     return app.test_client()
+
+@pytest.fixture(scope='function')
+def init_database(app):
+    with app.app_context():
+        db.create_all()
+        yield db
+        db.session.remove()
+        db.drop_all()
